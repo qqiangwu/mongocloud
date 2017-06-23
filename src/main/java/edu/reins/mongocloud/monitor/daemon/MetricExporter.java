048498644049062baeb2e9d37599a6786e12c8d1
wuqq <wqzhiep@gmail.com>
2017-06-23 15:00:43 +0800

    fix(monitor): fix endpoints and add extra labels

--- src/main/java/edu/reins/mongocloud/monitor/daemon/MetricExporter.java
+++ src/main/java/edu/reins/mongocloud/monitor/daemon/MetricExporter.java
@@ -6,6 +6,7 @@ import edu.reins.mongocloud.cluster.ClusterReport;
 import edu.reins.mongocloud.cluster.ShardedCluster;
 import edu.reins.mongocloud.instance.Instance;
 import edu.reins.mongocloud.instance.InstanceReport;
+import edu.reins.mongocloud.instance.InstanceType;
 import edu.reins.mongocloud.monitor.Monitor;
 import edu.reins.mongocloud.support.Units;
 import edu.reins.mongocloud.support.annotation.Nothrow;
@@ -18,33 +19,34 @@ import java.util.Optional;
 
 @Component
 public class MetricExporter {
-    private static final String LABEL_INSTANCE = "instance";
-    private static final String LABEL_CLUSTER = "cluster";
+    private static final String LABEL_INSTANCE_ID = "instance";
+    private static final String LABEL_INSTANCE_TYPE = "type";
+    private static final String LABEL_CLUSTER_ID = "cluster";
 
     private static final Gauge INSTANCE_CPU_USAGE = new Gauge.Builder()
             .name("instance_cpu_usage")
             .help("The cpu usage of a instance")
-            .labelNames(LABEL_INSTANCE)
+            .labelNames(LABEL_INSTANCE_ID, LABEL_INSTANCE_TYPE)
             .register();
     private static final Gauge INSTANCE_QPS = new Gauge.Builder()
             .name("instance_qps")
             .help("Instance reads per seconds")
-            .labelNames(LABEL_INSTANCE)
+            .labelNames(LABEL_INSTANCE_ID, LABEL_INSTANCE_TYPE)
             .register();
     private static final Gauge INSTANCE_TPS = new Gauge.Builder()
             .name("instance_tps")
             .help("Instance writes per seconds")
-            .labelNames(LABEL_INSTANCE)
+            .labelNames(LABEL_INSTANCE_ID, LABEL_INSTANCE_TYPE)
             .register();
     private static final Gauge CLUSTER_SHARDS = new Gauge.Builder()
             .name("cluster_shard_count")
             .help("Cluster shard counts")
-            .labelNames(LABEL_CLUSTER)
+            .labelNames(LABEL_CLUSTER_ID)
             .register();
     private static final Gauge CLUSTER_STORAGE = new Gauge.Builder()
             .name("cluster_storage_usage")
             .help("Cluster storage usage")
-            .labelNames(LABEL_CLUSTER)
+            .labelNames(LABEL_CLUSTER_ID)
             .register();
 
     @Autowired
@@ -72,11 +74,12 @@ public class MetricExporter {
     @Nothrow
     private void collectInstance(final Instance instance) {
         final String id = instance.getID().getValue();
+        final String type = instance.getType().toString();
         final InstanceReport report = instance.getReport();
 
-        INSTANCE_CPU_USAGE.labels(id).set(report.getCpuPercent());
-        INSTANCE_QPS.labels(id).set(report.getTotalReads());
-        INSTANCE_TPS.labels(id).set(report.getTotalWrites());
+        INSTANCE_CPU_USAGE.labels(id, type).set(report.getCpuPercent());
+        INSTANCE_QPS.labels(id, type).set(report.getTotalReads());
+        INSTANCE_TPS.labels(id, type).set(report.getTotalWrites());
     }
 
     @Nothrow
